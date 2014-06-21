2a2302350ac786c9ebbae811bb9ce2b77810e381
bhsu <bhsu22+github@gmail.com>
2014-06-21 10:45:40 -0400

    fixed array indexing

--- kruskal/ShapelyImp.py
+++ kruskal/ShapelyImp.py
@@ -1,9 +1,12 @@
 import numpy as np
 from numba import jit
 from math import factorial
-from .utils import *
 
 __all__ = ['Nvars','R2','ShapelyImp']
+
+from math import factorial
+import numpy as np
+from numba import jit, f8, i2, i8
 
 @jit('i8(i8)')
 def NVars(n):
@@ -15,36 +18,40 @@ def NVars(n):
     return count
 
 @jit('f8(f8[:,:],i8,i8)')
-def R2(S, i, n):
-    covariates = np.array([k+1 for k in range(n) if i &(1<<k)!=0])
-    Sxx_inv = np.linalg.inv( S[covariates,covariates] )
-    Syx = S[0,covariates]; Sxy = S[covariates,0]
+def R2(S,i, n):
+    
+    cov = np.zeros(n+1,dtype=bool)
+    for k in range(n):
+        if i&(1<<k)!=0: cov[k+1]=True
+    print bin(i)[2:].zfill(n),' ',cov
+    
+    Sxx = S[:,cov]
+    Sxx_inv = np.linalg.inv( Sxx[cov,:] )
+    Syx = S[0,cov]; Sxy = S[cov,0]
     return Syx.dot(Sxx_inv.dot(Sxy) )/S[0,0]
 
-@jit('f8[:](f8[:],f8[:,:],f8[:,:])')
-def ShapelyImp( Y, X, weights ):
-    
-    ### weighted covariance matrix - corrects scipy version
-    w = np.diagflat( weights/np.sum(weights),k=0)
-    X = np.sqrt(w).dot(X - np.sum( w.dot(X),axis=0))
-    S = 1./(1. - np.sum(w**2) ) * X.T.dot(X)
-
-    n = S.shape[1]
-    model = np.zeros(2**n)
-    kruskal = np.zeros(n)
+@jit('f8[:](f8[:,:])')
+def ShapelyImg( S ):
+    n_cov = S.shape[1]-1
+    model = np.zeros(2**n_cov)
+    kruskal=np.zeros(n_cov)
     
     model[0] = S[0,0]  ### no covariates in model, base case
-    for i in xrange(2**n):
-        if i%(2**n/2**10) == 0: print '%d combinations computed'%i
+    for i in xrange(2**n_cov):
+        #if i%(2**n/2**10) == 0: print '%d combinations computed'%i
 
         k = NVars(i)
-        if model[i] == 0: model[i] = R2(S,i,n)
+        if model[i] == 0: model[i] = R2(S,i,n_cov)
         
-        for ij in xrange(n):
+        for ij in xrange(n_cov):
             j = (1<<ij)
             if i == i|j: continue
-            if model[i|j] == 0:  model[i|j] = R2(S,i|j,n) 
+                
+            if model[i|j] == 0:  model[i|j] = R2(S,i|j,n_cov) 
+            #if ij==0:
+            #    print bin(i)[2:].zfill(n), bin(i|j)[2:].zfill(n), model[i], model[i|j]
+            #    print factorial(NVars(i)) * 1.*factorial(n - NVars(i)-1),' x ', (1.-model[i|j]/model[0])
             
-            kruskal[ ij ] += factorial(k) * 1.*factorial(n - k -1)/factorial(n)* (model[i]-model[i|j])/model[0]
+            kruskal[ ij ] += factorial(k) * 1.*factorial(n_cov - k -1)/factorial(n_cov)* (model[i]-model[i|j])/model[0]
             
     return kruskal
